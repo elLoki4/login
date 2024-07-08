5695edda98a8c1346e597e85fec92838abe95d9e
elLoki4 <133483704+elLoki4@users.noreply.github.com>
2024-07-08 01:07:24 -0300

    Add files via upload

---
+++ dist/sandbox/765.22420c611a6c3531.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksandbox=self.webpackChunksandbox||[]).push([[765],{6765:(d,n,e)=>{e.r(n),e.d(n,{ReportesModule:()=>t});var u=e(9535),s=e(1076);let t=(()=>{class o{}return o.\u0275fac=function(a){return new(a||o)},o.\u0275mod=s.oAB({type:o}),o.\u0275inj=s.cJS({imports:[u.ez]}),o})()}}]);
